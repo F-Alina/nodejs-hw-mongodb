12aa992abb84f6b30a8e0e1c1cab5bbc58cadc35
Alina Furda <alina.furda1806@gmail.com>
2025-09-24 16:42:35 +0300

    hw04-validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,23 +8,37 @@ import {
 } from '../controllers/contacts.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 contactsRouter.get(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
-contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+contactsRouter.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 contactsRouter.patch(
   '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
 contactsRouter.delete(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(deleteContactController),
 );
 
